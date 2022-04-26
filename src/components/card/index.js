556d683a1624648f4108097e02432e97ac6427f6
Saya Konami <kaigth@hotmail.com>
2022-04-26 08:53:18 -0700

    updated util in card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -3,23 +3,9 @@ import PropTypes from 'prop-types';
 import { Rating } from 'react-simple-star-rating';
 
 import Heart from '../svg/heart';
+import { renderCost } from '../../util';
 
 import './styles.scss';
-
-/**
- * Render cost symbol based on google price rating
- *
- * @function
- * @memberof components
- * @param {object} cost The google cost rating
- * @returns {string} The symbol based on the google cost rating
- */
-const renderCost = (cost) => {
-  if (cost === 2) return '$$';
-  if (cost === 3) return '$$$';
-
-  return '$';
-}
 
 /**
  * Render a basic card
